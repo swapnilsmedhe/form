06a1aa8ce766c03e7543ec402a7497e4bab9d805
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-06-06 18:19:52 +0530

    Validate date of birth

--- form.js
+++ form.js
@@ -39,17 +39,21 @@ const areAllDigits = (string) => {
 };
 
 const isNameValid = (name) => {
-  return name.length < 5 ? false : areAllAlphabets(name);
+  return name.length > 4 && areAllAlphabets(name);
 };
 
-const isDateValid = (date) => true;
+const isDateValid = (date) => /^\d{4}-\d{2}-\d{2}$/.test(date);
+
 const areHobbiesValid = (hobbies) => hobbies.length > 0;
+
 const isPhoneNumValid = (phoneNumber) => {
-  return phoneNumber.length !== 10 ? false : areAllDigits(phoneNumber);
+  return phoneNumber.length === 10 && areAllDigits(phoneNumber);
 };
 
 const main = () => {
-  const messages = ['Please enter your name', 'Please enter your DOB', 'Please enter your hobbies', 'Enter phone number', 'Thank you'];
+  const messages = [
+    'Please enter your name', 'Please enter your DOB', 'Please enter your hobbies', 'Enter phone number', 'Thank you'
+  ];
   const validators = [
     isNameValid, isDateValid, areHobbiesValid, isPhoneNumValid
   ];
